0a7abf4408256fb3d85205368784f62f72e51ee2
Songmu <y.songmu@gmail.com>
2016-04-30 22:46:58 +0900

    err

--- example_test.go
+++ example_test.go
@@ -2,13 +2,18 @@ package gitmock_test
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/Songmu/gitmock"
 )
 
 func ExampleNew() {
-	gm, _ := gitmock.New()
+	gm, err := gitmock.New()
+	if err != nil {
+		log.Print(err)
+		return
+	}
 	repo := gm.RepoPath()
 	defer os.RemoveAll(repo)
 	gm.Init()
